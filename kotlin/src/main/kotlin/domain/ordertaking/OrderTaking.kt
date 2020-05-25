e201647a096160d1bd92cd2136da43e556bed126
Amit Dawle <amit.dawle@gmail.com>
2020-05-25 15:40:01 +0100

    Model in kotlin with unknown types

--- kotlin/src/main/kotlin/domain/ordertaking/OrderTaking.kt
+++ kotlin/src/main/kotlin/domain/ordertaking/OrderTaking.kt
@@ -1,2 +1,15 @@
-package domain
+package domain.ordertaking
 
+class CustomerId(val customerId: Int)
+class WidgetCode(val widgetCode: String)
+class UnitQuantity(val unitQuantity: Int)
+class KilogramQuantity(val kilogramQuantity: Float)
+
+
+data class Order(
+    val customerInfo : Nothing,
+    val shippingAddress : Nothing,
+    val billingAddress : Nothing,
+    val orderLines : Nothing,
+    val AmountToBill : Nothing
+)
